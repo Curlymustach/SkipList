b6ccff836ecb165a009eef909ec5e2ddf7fc3091
Meghana Sagare <meghanasagare@gmail.com>
2018-06-17 10:43:56 -0700

    Initial Commit

---
+++ SkipList/SkipList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkipList
+{
+    class SkipList<T> : ICollection<T>, IEnumerable<T>
+    {
+
+        public double probablity = 0.5;
+        Random rand;
+
+        public SkipList()
+        {
+
+        }
+
+        public void Add(T value)
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public void CopyTo(T[] array, int index)
+        {
+
+        }
+
+        public bool Contains(T value)
+        {
+            return false;
+        }
+
+        public bool Remove(T value)
+        {
+            return false;
+        }
+
+        public T getEnumerator()
+        {
+            
+        }
+
+        public int Count()
+        {
+
+        }
+
+        public bool isReadOnly()
+        {
+        }
+    
+}
